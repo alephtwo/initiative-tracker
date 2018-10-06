140cf74eeafd7a57ba63890e4d481554e7944ca6
Ben Christianson <thhuntertgm@gmail.com>
2018-10-06 15:59:45 -0500

    Buttons!

--- src/components/InitiativeTracker.tsx
+++ src/components/InitiativeTracker.tsx
@@ -5,30 +5,41 @@ import { List } from 'immutable'
 import InitiativeBlock from './InitiativeBlock'
 import { Initiative } from '../interfaces/Initiative'
 import { Dispatch } from 'redux'
-import { Paper, Grid, Theme, withStyles } from '@material-ui/core'
+import { Paper, Grid, Theme, withStyles, Button, StyledComponentProps } from '@material-ui/core'
 
-interface Props extends DispatchProp {
+interface Props extends DispatchProp, StyledComponentProps {
   initiatives: List<Initiative>
 }
 
-const InitiativeTracker = (props: Props) => (
-  <div>
-    <Grid container spacing={16}>
-      <Grid item xs={6}>
-        <Paper>
-          <a href='#' onClick={() => props.dispatch({ type: 'ADD_BLOCK' })}>
-            Add
-          </a>
+const InitiativeTracker = (props: Props) => {
+  const { classes = {} } = props
 
-          <a href='#' onClick={() => props.dispatch({ type: 'SORT_BY_INITIATIVE' })}>
-            Sort
-          </a>
-        </Paper>
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <Grid container spacing={16}>
+        <Grid item xs={6}>
+          <Paper>
+            <Button
+              className={classes.button}
+              color='primary'
+              variant='contained'
+              onClick={() => props.dispatch({ type: 'ADD_BLOCK' })}>
+                Add
+            </Button>
+            <Button
+              className={classes.button}
+              color='secondary'
+              variant='contained'
+              onClick={() => props.dispatch({ type: 'SORT_BY_INITIATIVE' })}>
+                Sort
+            </Button>
+          </Paper>
+        </Grid>
       </Grid>
-    </Grid>
-    {generateBlocks(props.dispatch, props.initiatives)}
-  </div>
-)
+      {generateBlocks(props.dispatch, props.initiatives)}
+    </div>
+  )
+}
 
 const generateBlocks = (dispatch: Dispatch, initiatives: List<Initiative>) =>
   initiatives.map((i: Initiative) => generateBlock(dispatch, i)).toJS()
@@ -48,7 +59,14 @@ const generateBlock = (dispatch: Dispatch, initiative: Initiative) => {
   )
 }
 
+const styles = (theme: any) => ({
+  button: {
+    margin: theme.spacing.unit
+  }
+})
+
 const mapStateToProps = (state: AppState) =>
   ({ initiatives: state.get('initiatives') })
 
-export default connect(mapStateToProps)(InitiativeTracker)
+const StyledInitiativeTracker = withStyles(styles)(InitiativeTracker)
+export default connect(mapStateToProps)(StyledInitiativeTracker)
