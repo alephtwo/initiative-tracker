17a2c5d9c879b4bfa1808496d610f40c34ab30b0
Ben Christianson <thhuntertgm@gmail.com>
2021-03-17 00:50:24 -0500

    Create callbacks

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,29 +1,31 @@
 import * as React from 'react';
 import { useReducer } from 'react';
-import { reducer, State, Message } from '../logic/reducer';
+import { Message, reducer, State } from '../logic/reducer';
 import { Card } from './Card';
 
 const initialState = new State();
 
 function Application() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const actions = createActions(dispatch);
+  const callbacks = createCallbacks(dispatch);
 
   const cards = state.participants.map((p) => {
-    return <Card key={p.id} participant={p} deleteRow={() => dispatch({ type: 'delete-row', id: p.id })} />;
+    return <Card key={p.id} dispatch={dispatch} participant={p} />;
   });
 
   return (
     <>
-      <button onClick={actions.addRow}>Add Row</button>
+      <button onClick={callbacks.addRow}>Add Row</button>
       <hr />
       {cards}
     </>
   );
 }
 
-const createActions = (dispatch: React.Dispatch<Message>) => ({
-  addRow: () => dispatch({ type: 'add-row' }),
-});
+function createCallbacks(dispatch: React.Dispatch<Message>) {
+  return {
+    addRow: () => dispatch({ type: 'add-row' }),
+  };
+}
 
 export default Application;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,28 @@
 import * as React from 'react';
 import { Participant } from '../data/Participant';
+import { Message } from '../logic/reducer';
 
 interface CardProps {
+  dispatch: React.Dispatch<Message>;
   participant: Participant;
-  deleteRow: () => void;
 }
 
 export function Card(props: CardProps): JSX.Element {
-  const { participant, deleteRow } = props;
+  const { dispatch, participant } = props;
+  const callbacks = createCallbacks(dispatch, participant.id);
+
   return (
     <div>
       <input type="text" value={participant.name} />
       <input type="text" value={participant.initiative} />
       <input type="text" value={participant.hp} />
-      <button onClick={deleteRow}>Delete</button>
+      <button onClick={callbacks.deleteRow}>Delete</button>
     </div>
   );
+}
+
+function createCallbacks(dispatch: React.Dispatch<Message>, id: string) {
+  return {
+    deleteRow: () => dispatch({ type: 'delete-row', id: id }),
+  };
 }
